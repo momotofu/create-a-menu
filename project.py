886236c24a1c452982d1282fb5957d786533e58d
Christopher Reece <mrkita.reece@gmail.com>
2018-02-19 11:00:28 +0900

    added create new menu item functionality

--- project.py
+++ project.py
@@ -1,11 +1,122 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello')
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
-def HelloWorld():
-    return "Hello World"
+from html_builder import HTML_Builder as HB
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+
+    output = HB()
+
+    output.add_html("""
+        <h1>%s</h1>
+        """ % restaurant.name
+        )
+
+    output.add_html("""
+        <a href='%snew-item'>create a new item</a>
+        """ % request.path
+        )
+
+    for item in items:
+        output.add_html("""
+            <h4> %s </h4>
+            <p> %s </p>
+            <p> %s </p>
+            <a href=%sedit>edit</a>
+            <a href=%sdelete>delete</a>
+            </br>
+        """ % (item.name, item.price, item.description, request.path,
+            request.path)
+        )
+
+    return output.get_html()
+
+
+# Task 1: Create route for newMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/new-item/', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'GET':
+        output = HB()
+        output.add_html("""
+            <h1> Create a new menu item for %s </h1>
+            <form method='POST' action='%s'>
+                <label>Name:
+                    <input name='name'>
+                </label>
+                <br />
+                <label>Price:
+                    <input name='price'>
+                </label>
+                <br />
+                <label>Description:
+                    <input name='description'>
+                </label>
+                <br />
+                <label>Course:
+                    <input name='course'>
+                </label>
+                <br />
+                <input type='submit' value='CREATE'>
+            </form>
+            """ % (restaurant.name, request.path)
+            )
+        return output.get_html()
+
+    if request.method == 'POST':
+        params = request.form
+        try:
+            item = MenuItem(
+                    name=params['name'],
+                    price=params['price'],
+                    course=params['course'],
+                    description=params['description'],
+                    restaurant=restaurant
+                    )
+            session.add(item)
+            session.commit()
+
+            output = HB()
+            output.add_html("""
+                <h1>Successfuly create %s </h1>
+                <a href="%s">Back to %s</a>
+                """ % (params['name'], request.path, restaurant.name)
+                )
+            return output.get_html()
+
+        except:
+            session.rollback()
+            raise
+            output = HB()
+            output.add_html("""
+                <h1>Failed to create new menu item</h1>
+                <a href=%s> try again </a>
+                <a href='/retaurants> back to restaurants </a>
+                """ % request.path
+                )
+            return output.get_html()
+
+# Task 2: Create route for editMenuItem function here
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
