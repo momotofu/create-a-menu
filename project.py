3379a535e479ec26011e160ccb5dcac6827bbc2a
Christopher Reece <mrkita.reece@gmail.com>
2018-02-20 09:54:46 +0900

    added flashing for created menu items

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template, url_for, flash
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
