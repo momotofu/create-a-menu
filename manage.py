ea724545f1a9c9c450ff9372b09cbe81a3986ad1
Christopher Reece <mrkita.reece@gmail.com>
2018-03-07 09:44:38 +0900

    removed manage.py

--- manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(url='sqlite:///test_restaurantmenu.db', debug='False', repository='migrations')
