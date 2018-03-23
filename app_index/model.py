7fb7a3c64f344f67924ebc1487494bd543d27cb5
Christopher Reece <mrkita.reece@gmail.com>
2018-03-24 06:52:08 +0900

    added password hash to user_model

--- app_index/model.py
+++ app_index/model.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
 
@@ -15,6 +16,13 @@ class User(Base):
     name = Column('name', String(80), nullable=False)
     picture = Column('picture', String(80), nullable=False)
     email = Column('email', String(80))
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
     @property
     def serialize(self):
